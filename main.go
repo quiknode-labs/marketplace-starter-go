a482e22af4b4896228827ba37d1a369b5de4b528
luccastera <luc.castera@gmail.com>
2023-03-15 13:06:29 +0100

    Added basic auth for provision endpoints

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/quiknode-labs/qn-go-add-on/controllers"
 	"github.com/quiknode-labs/qn-go-add-on/initializers"
 
@@ -15,10 +17,14 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.POST("/provision", controllers.Provision)
-	r.DELETE("/deprovision", controllers.Deprovision)
-	r.PUT("/update", controllers.Update)
-	r.DELETE("/deactivate_endpoint", controllers.DeactivateEndpoint)
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		os.Getenv("BASIC_AUTH_USERNAME"): os.Getenv("BASIC_AUTH_PASSWORD"),
+	}))
+
+	authorized.POST("/provision", controllers.Provision)
+	authorized.DELETE("/deprovision", controllers.Deprovision)
+	authorized.PUT("/update", controllers.Update)
+	authorized.DELETE("/deactivate_endpoint", controllers.DeactivateEndpoint)
 
 	r.POST("/rpc", controllers.RPC)
 
