b2de210c476dfff3201f6a6bb7a12b3ac0f87c7a
luccastera <luc.castera@gmail.com>
2023-03-15 12:49:53 +0100

    healthcheck now makes a call to DB

--- controllers/healthcheckController.go
+++ controllers/healthcheckController.go
@@ -2,9 +2,14 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/quiknode-labs/qn-go-add-on/initializers"
 )
 
 func Healthcheck(c *gin.Context) {
+	// make a call to DB to make sure it's up
+	var count int64
+	initializers.DB.Table("accounts").Count(&count)
+
 	c.JSON(200, gin.H{
 		"status": "ok",
 	})
